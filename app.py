0830a5dd10f69579b5c14d3f18246024fa62cb42
Ephemerill <gabrieljlosh@gmail.com>
2024-10-16 14:18:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from scrape_menu import scrape_menu
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Welcome to Your Flask Web Page!</h1>"
+    return render_template('index.html')
+
+@app.route('/scrape')
+def scrape():
+    menu_data = scrape_menu()
+    return render_template('menu.html', menu=menu_data)
 
 if __name__ == '__main__':
     app.run()
